3bfa803dfafde1f84b572c92296a16cc6c791fcd
zhouyu <rudy_zhou@sian.cn>
2021-02-05 09:29:11 +0800

    格式化修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
+    "plugin:vue/recommended",
     '@vue/standard'
   ],
   parserOptions: {
@@ -12,6 +13,45 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/order-in-components": [
+      "error",
+      {
+        "order": [
+          "el",
+          "name",
+          "key",
+          "parent",
+          "functional",
+          ["delimiters", "comments"],
+          ["components", "directives", "filters"],
+          "extends",
+          "mixins",
+          ["provide", "inject"],
+          "ROUTER_GUARDS",
+          "layout",
+          "middleware",
+          "validate",
+          "scrollToTop",
+          "transition",
+          "loading",
+          "inheritAttrs",
+          "model",
+          ["props", "propsData"],
+          "emits",
+          "setup",
+          "fetch",
+          "asyncData",
+          "data",
+          "head",
+          "computed",
+          "watch",
+          "watchQuery",
+          "methods",
+          ["template", "render"],
+          "renderError"
+        ]
+      }
+    ],
   }
 }
